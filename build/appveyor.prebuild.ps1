71bf2066215ddd00835fa0492426f9520a005333
Christopher Bennage <bennage@microsoft.com>
2015-05-20 18:30:10 -0700

    prebuild script for appveyor

---
+++ build/appveyor.prebuild.ps1
@@ -0,0 +1,10 @@
+﻿ echo "Creating a local config from template for RunFromConsole projects"
+ 
+ $templateFile = "mysettings-template.config"
+ $configFile = "mysettings.config"
+ $configFolder = "src\RunFromConsole\"
+
+ $srcPath = Join-Path $configFolder $templateFile
+ $dstPath = Join-Path $configFolder $configFile
+ 
+ Copy-Item -Path $srcPath -Destination $dstPath
