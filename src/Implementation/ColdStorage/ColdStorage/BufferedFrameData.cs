09a8eb303c77c522c1bcdeb38e52a2e6c8a5bead
Francis Cheung <fcheung@microsoft.com>
2015-05-28 14:18:44 -0700

    Adding disposing logic to BufferedFrameData and its consumer.

--- src/Implementation/ColdStorage/ColdStorage/BufferedFrameData.cs
+++ src/Implementation/ColdStorage/ColdStorage/BufferedFrameData.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using Microsoft.Practices.IoTJourney.ColdStorage.RollingBlobWriter;
 using Microsoft.ServiceBus.Messaging;
 
 namespace Microsoft.Practices.IoTJourney.ColdStorage
 {
-    public class BufferedFrameData : BlockData
+    public class BufferedFrameData : BlockData, IDisposable
     {
         private readonly EventData _lastEventDataInFrame;
 
@@ -22,5 +23,20 @@ namespace Microsoft.Practices.IoTJourney.ColdStorage
         {
             get { return _lastEventDataInFrame; }
         }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_lastEventDataInFrame != null) _lastEventDataInFrame.Dispose();
+            }
+        }
+
     }
 }
