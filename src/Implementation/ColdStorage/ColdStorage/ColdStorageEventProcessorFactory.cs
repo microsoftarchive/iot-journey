0708b49cf8265d3f8355b8becf60f15a1b5838e0
Francis Cheung <fcheung@microsoft.com>
2015-05-27 17:00:53 -0700

    #54. Ported ColdStorage Event Hub EventProcessor from Data Pipeline project

---
+++ src/Implementation/ColdStorage/ColdStorage/ColdStorageEventProcessorFactory.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using Microsoft.Practices.IoTJourney.ColdStorage.RollingBlobWriter;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.Practices.IoTJourney.ColdStorage
+{
+    public class ColdStorageEventProcessorFactory : IEventProcessorFactory
+    {
+        private readonly Func<string, IBlobWriter> _blobWriterFactory = null;
+        private readonly CancellationToken _token = CancellationToken.None;
+        private readonly int _warningLevel;
+        private readonly int _tripLevel;
+        private readonly TimeSpan _stallInterval;
+        private readonly TimeSpan _logCooldownInterval;
+        private readonly string _eventHubName;
+
+        public ColdStorageEventProcessorFactory(
+            Func<string, IBlobWriter> blobWriterFactory,
+            CancellationToken token,
+            int warningLevel,
+            int tripLevel,
+            TimeSpan stallInterval,
+            TimeSpan logCooldownInterval,
+            string eventHubName)
+        {
+
+            Guard.ArgumentNotNull(blobWriterFactory, "blobWriterFactory");
+
+            _token = token;
+            _blobWriterFactory = blobWriterFactory;
+            _warningLevel = warningLevel;
+            _tripLevel = tripLevel;
+            _stallInterval = stallInterval;
+            _logCooldownInterval = logCooldownInterval;
+            _eventHubName = eventHubName;
+        }
+
+        public IEventProcessor CreateEventProcessor(PartitionContext context)
+        {
+            var processor = new ColdStorageProcessor(
+                _blobWriterFactory,
+                _token,
+                _warningLevel,
+                _tripLevel,
+                _stallInterval,
+                _logCooldownInterval, 
+                _eventHubName
+            );
+            return processor;
+        }
+    }
+}
