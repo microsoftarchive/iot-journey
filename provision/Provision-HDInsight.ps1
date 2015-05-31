ecf7b9d17009e1967013d2644d4158c167c8c167
Hernan Veiras <hernan.veiras@gmail.com>
2015-05-31 16:13:59 -0300

    Added check for existing HDInsight cluster.

--- provision/Provision-HDInsight.ps1
+++ provision/Provision-HDInsight.ps1
@@ -13,9 +13,9 @@ Param (
     [Parameter(Mandatory=$True)][String]$StorageAccountName,
 
     [ValidatePattern("^[A-Za-z][-A-Za-z0-9]*[A-Za-z0-9]$")] #needs to start with letter or number, and contain only letters, numbers, and hyphens.
-    [Parameter(Mandatory=$True)][string]$StorageContainerName,
+    [Parameter(Mandatory=$False)][string]$StorageContainerName = "iot-hdicontainer01",
 
-    [Parameter(Mandatory=$True)][string]$ClusterName,
+    [Parameter(Mandatory=$False)][string]$ClusterName = "iot-hdicluster01",
     [Parameter(Mandatory=$False)][int]$ClusterNodes = 2,
     [Parameter(Mandatory=$False)][string]$Location = "Central US"
 )
@@ -40,17 +40,30 @@ $DestContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -
 $CurrentStorageContainer = Get-AzureStorageContainer -Name $StorageContainerName -Context $DestContext -ErrorAction SilentlyContinue
 # Create a Blob storage container if it does not exists.
 if (!$CurrentStorageContainer)
-{  
+{  s
     New-AzureStorageContainer -Name $StorageContainerName -Context $DestContext
 }
 
-$Credential = Get-Credential -UserName "admin" -Message "Provide a password for the new cluster. The filed must contain: a lowercase letter, a number and a special character."
+$HDInsightCluster = Get-AzureHDInsightCluster -Name $ClusterName
 
-# Create a new HDInsight cluster
-New-AzureHDInsightCluster -Name $ClusterName -Location $Location `
-                          -DefaultStorageAccountName "$StorageAccountName.blob.core.windows.net" `
-                          -DefaultStorageAccountKey $StorageAccountKey `
-                          -DefaultStorageContainerName $ClusterName `
-                          -ClusterSizeInNodes $ClusterNodes `
-                          -Credential $Credential
+if(!$HDInsightCluster)
+{
+    Write-Verbose "Creating a new HDInsight cluser named: [$ClusterName]. This operation may take several minutes to complete."
+
+    $Credential = Get-Credential -UserName "admin" -Message "Provide a password for the new cluster. The filed must contain: a lowercase letter, a number and a special character."
+
+    # Create a new HDInsight cluster
+    New-AzureHDInsightCluster -Name $ClusterName -Location $Location `
+                              -DefaultStorageAccountName "$StorageAccountName.blob.core.windows.net" `
+                              -DefaultStorageAccountKey $StorageAccountKey `
+                              -DefaultStorageContainerName $ClusterName `
+                              -ClusterSizeInNodes $ClusterNodes `
+                              -Credential $Credential
+}
+else
+{
+    Write-Verbose "An HDInsight cluster named: [$ClusterName] already exists."
+}
+
+
                           
