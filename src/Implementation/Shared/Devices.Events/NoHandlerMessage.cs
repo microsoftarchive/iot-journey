d5529f52134274ba39f9512108bef6682e949191
larry brader <lbrader@microsoft.com>
2015-06-01 14:53:50 -0700

    Removed unused scenarios EventsWithNoHandler, MalformedEvents, LongRunningEvents #69

--- src/Implementation/Shared/Devices.Events/NoHandlerMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-namespace Microsoft.Practices.IoTJourney.Devices.Events
-{
-    // This event does not have a corresponding handler.
-    // It is used to demonstrate how the dispatcher will
-    // respond when a handler cannot be resolved.
-    public class NoHandlerEvent { }
-}
