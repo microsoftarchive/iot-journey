fabe6c01fb883d552138b879ed42c78a3a46a3b3
Matt Johnson <matt.johnson@microsoft.com>
2015-06-17 18:07:46 -0700

    Add support for command line options

--- src/RunFromConsole/ScenarioSimulator.ConsoleHost/Program.cs
+++ src/RunFromConsole/ScenarioSimulator.ConsoleHost/Program.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Diagnostics.Tracing;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ namespace Microsoft.Practices.IoTJourney.ScenarioSimulator.ConsoleHost
 {
     internal class Program
     {
+        private static FileSystemWatcher _fileSystemWatcher;
+
         private static void Main(string[] args)
         {
             var observableEventListener = new ObservableEventListener();
@@ -26,6 +29,22 @@ namespace Microsoft.Practices.IoTJourney.ScenarioSimulator.ConsoleHost
 
             var deviceSimulator = new SimulationProfile("Console", 1, configuration);
 
+            // check for scenario specified on the command line
+            if (args.Length > 0)
+            {
+                var scenario = args.Contains("/default", StringComparer.OrdinalIgnoreCase)
+                    ? SimulationScenarios.DefaultScenario()
+                    : args.First(x => !x.StartsWith("/"));
+
+                var ct = args.Contains("/webjob", StringComparer.OrdinalIgnoreCase)
+                    ? GetWebJobCancellationToken()
+                    : CancellationToken.None;
+
+                deviceSimulator.RunSimulationAsync(scenario, ct).Wait(); // todo, use await after #138 is merged
+                return;
+            }
+
+            // no command line arguments, so prompt with a menu
             var options = SimulationScenarios
                 .AllScenarios
                 .ToDictionary(
@@ -33,6 +52,27 @@ namespace Microsoft.Practices.IoTJourney.ScenarioSimulator.ConsoleHost
                     scenario => (Func<CancellationToken, Task>)(token => deviceSimulator.RunSimulationAsync(scenario, token)));
 
             Tests.Common.ConsoleHost.WithOptions(options);
+        }
+        
+        private static CancellationToken GetWebJobCancellationToken()
+        {
+            // See: http://blog.amitapple.com/post/2014/05/webjobs-graceful-shutdown
+
+            var shutdownFile = Environment.GetEnvironmentVariable("WEBJOBS_SHUTDOWN_FILE");
+            var directory = Path.GetDirectoryName(shutdownFile);
+            if (directory == null)
+                return CancellationToken.None;
+
+            var cts = new CancellationTokenSource();
+            _fileSystemWatcher = new FileSystemWatcher(directory);
+            _fileSystemWatcher.Created += (sender, args) =>
+            {
+                if (args.FullPath.Equals(Path.GetFullPath(shutdownFile), StringComparison.OrdinalIgnoreCase))
+                    cts.Cancel();
+            };
+
+            _fileSystemWatcher.EnableRaisingEvents = true;
+            return cts.Token;
         }
     }
 }
---
+++ src/RunFromConsole/ScenarioSimulator.ConsoleHost/WebJob.cmd
@@ -0,0 +1,1 @@
+Microsoft.Practices.IoTJourney.ScenarioSimulator.ConsoleHost.exe /default /webjob
