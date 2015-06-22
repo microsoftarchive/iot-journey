087c57115441e3e9634e83a021b7205e9b966001
Hernan Veiras <hernan.veiras@gmail.com>
2015-06-22 17:39:56 -0300

    [WIP] created device provision solution.

---
+++ src/Implementation/DeviceProvision/DeviceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Practices.IoTJourney.DeviceProvision
+{
+    public class DeviceInfo
+    {
+        public string DeviceId { get; set; }
+
+        public string BuildingId { get; set; }
+
+        public string Token { get; set; }
+    }
+}
