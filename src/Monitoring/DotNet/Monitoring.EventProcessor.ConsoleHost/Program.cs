c7b636228f8bf2ed1a0de2d21469d90ff1e474fe
Christopher Bennage <bennage@microsoft.com>
2015-09-16 13:27:15 -0700

    comments in the console host

--- src/Monitoring/DotNet/Monitoring.EventProcessor.ConsoleHost/Program.cs
+++ src/Monitoring/DotNet/Monitoring.EventProcessor.ConsoleHost/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Globalization;
 using System.IO;
 using Microsoft.Practices.IoTJourney.Monitoring.EventProcessor.ConsoleHost.Formatters;
 using Microsoft.Practices.IoTJourney.Monitoring.EventProcessor.ConsoleHost.Sinks;
@@ -12,23 +13,24 @@ namespace Microsoft.Practices.IoTJourney.Monitoring.EventProcessor.ConsoleHost
     {
         private static void Main(string[] args)
         {
+            // This is mean to be an example of how you can
+            // use the monitor. It can be hosted in a console app,
+            // web app, or whatever works for your situation.
             var configuration = Configuration.GetCurrentConfiguration();
             var monitor = EventHubMonitorFactory.CreateAsync(configuration).Result;
 
-            var formatter = new CsvEventTextFormatter();
-            var filename = string.Format(
-                "Data\\{0}-{1}.csv", configuration.ConsumerGroupName,
-                DateTime.Now.ToString("yyyy-MM-dd-hh-mm"));
+            // We want to write out the data we collect to a
+            // flat file, formatted as CSV. 
+            // We can import this file into some tool, 
+            // e.g. Excel or PowerBI, for visualization..
+            var flatFileSink = CreateFlatFileSink(configuration.ConsumerGroupName);
 
-            var sink = new FlatFileSink(filename, formatter, true);
+            // The monitor allows for multiple subscribers.
+            // First, we'll subscribe the flat file sink.
+            monitor.Subscribe(flatFileSink);
 
-            var outputDir = Path.Combine(Environment.CurrentDirectory, "Data");
-            if (!Directory.Exists(outputDir))
-            {
-                Directory.CreateDirectory(outputDir);
-            }
-
-            monitor.Subscribe(sink);
+            // Now, we'll have a second subscription that 
+            // simply writes the data to the console.
             monitor.Subscribe(snapshot =>
             {
                 Console.WriteLine("Partition {0}", snapshot.PartitionId);
@@ -37,7 +39,7 @@ namespace Microsoft.Practices.IoTJourney.Monitoring.EventProcessor.ConsoleHost
                 Console.WriteLine("- LastCheckpointTimeUtc: {0}", snapshot.LastCheckpointTimeUtc);
                 Console.WriteLine("- LastEnqueuedTimeUtc: {0:}", snapshot.LastEnqueuedTimeUtc);
                 Console.WriteLine("- UnprocessedEvents: {0}", snapshot.UnprocessedEvents);
-                Console.WriteLine("");
+                Console.WriteLine();
             },
             e =>
             {
@@ -49,6 +51,25 @@ namespace Microsoft.Practices.IoTJourney.Monitoring.EventProcessor.ConsoleHost
             });
 
             Console.ReadKey();
+        }
+
+        private static FlatFileSink CreateFlatFileSink(string consumerGroupName)
+        {
+            var formatter = new CsvEventTextFormatter();
+            var filename = string.Format(
+                CultureInfo.InvariantCulture,
+                "Data\\{0}-{1}.csv",
+                consumerGroupName,
+                DateTime.Now.ToString("yyyy-MM-dd-hh-mm"));
+
+            var sink = new FlatFileSink(filename, formatter, true);
+
+            var outputDir = Path.Combine(Environment.CurrentDirectory, "Data");
+            if (!Directory.Exists(outputDir))
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+            return sink;
         }
     }
 }
